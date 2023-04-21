3e6ea92405954f73f5260bf3640773da36eea040
DragonGong <dragongong20040318@gmail.com>
2023-04-21 10:39:14 +0800

    it is a test

--- LearningJavaSE/src/Main.java
+++ LearningJavaSE/src/Main.java
@@ -1,6 +1,5 @@
 public class Main{
     public static void main(String[] args) {
-        System.out.println("you are a woman");
-        System.out.println("you are a man");
+        System.out.println("people");
     }
 }
